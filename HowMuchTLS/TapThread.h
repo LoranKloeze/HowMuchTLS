28ca7ca22cb35c446e6a40d96a7ba2f10ed14609
Loran Kloeze <loran@ralon.nl>
2017-06-20 10:41:14 +0200

    Add license stuff

--- HowMuchTLS/TapThread.h
+++ HowMuchTLS/TapThread.h
@@ -1,3 +1,13 @@
+/*
+HowMuchTLS
+Simple program to check the amount of port 80 traffic compared to port 443 traffic
+20-06-2017
+(c) 2017 - Loran Kloeze - loran@ralon.nl
+
+https://github.com/LoranKloeze/WhatsAllApp
+
+*/
+
 #pragma once
 #include <QThread>
 #include "pcap.h"
